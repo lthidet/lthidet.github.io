af274ad4e10821a2723e63699b040d24f812942c
Louis Thidet <louis.thidet@protonmail.com>
2025-05-29 01:41:27 +0200

    initial commit

---
+++ data/projets.js
@@ -0,0 +1,64 @@
+const projectsData = [
+    {
+        title: "BOMNET",
+        title_en: "BOMNET",
+        description: "Plug-in pour OMNeT++ générant des environnements en 3D à partir de fichiers BIM. Présenté à l'IEEE GLOBECOM 2024.",
+        description_en: "OMNeT++ plug-in generating 3D environments from BIM files. Presented at IEEE GLOBECOM 2024.",
+        imageUrl: "img/projets/bomnet.jpg",
+        tags: ["BIM", "Eclipse", "Flask"],
+        tags_en: ["BIM", "TensorFlow", "Flask"],
+        publicationUrl: "https://ieeexplore.ieee.org/document/10900994",
+        codeUrl: "https://github.com/bomnet/bomnet"
+    },
+    {
+        title: "xIngestor",
+        title_en: "xIngestor",
+        description: "Application microservices tout-en-un de gestion de noeuds IoT et de traitement et visualisation de données, basée sur ses ses propres firmwares.",
+        description_en: "All-in-one microservices application for IoT node management, data processing, and visualization, based on its own firmware.",
+        imageUrl: "img/projets/xingestor.jpg",
+        tags: ["Spring", "Microservices", "MQTT", "IoT"],
+        tags_en: ["Spring", "Microservices", "MQTT", "IoT"],
+    },
+    {
+        title: "Marché de l'immobilier",
+        title_en: "Real Estate Market",
+        description: "Visualisation interactive avec filtres temporels et géographiques des ventes et prix de l'immobilier dans le Grand Paris.",
+        description_en: "Interactive visualization with temporal and geographical filters of real estate sales and prices in Greater Paris.",
+        imageUrl: "img/projets/immobilier.png",
+        tags: ["R", "Shiny", "Géomatique", "Dataviz"],
+        tags_en: ["R", "Shiny", "Geomatics", "Dataviz"],
+        demoUrl: "https://louis-thidet.shinyapps.io/thidet_louis_exo2a",
+        codeUrl: "https://github.com/lthidet/application_m2_parisien"
+    },
+    {
+        title: "Flux ferroviaire",
+        title_en: "Railway Flows",
+        description: "Visualisation interactive analysant les flux du réseau ferré d'Île-de-France.",
+        description_en: "Interactive visualization analyzing railway flows in the Île-de-France network.",
+        imageUrl: "img/projets/ferroviaire.png",
+        tags: ["R", "Shiny", "Dataviz", "Leaflet"],
+        tags_en: ["R", "Shiny", "Dataviz", "Leaflet"],
+        demoUrl: "https://louis-thidet.shinyapps.io/thidet_ridership_patterns_idf",
+        codeUrl: "https://github.com/lthidet/ridership_patterns_ile_de_france"
+    },
+    {
+        title: "Déplacement scolaires",
+        title_en: "School Mobility",
+        description: "Application d'analyse des déplacements scolaires intercommunaux au sein de l'Académie de Créteil.",
+        description_en: "Application for analyzing intermunicipal school commuting patterns within the Créteil school district.",
+        imageUrl: "img/projets/scolaire.png",
+        tags: ["R", "Shiny", "Géomatique", "Dataviz"],
+        tags_en: ["R", "Shiny", "Geomatics", "Dataviz"],
+        demoUrl: "https://louis-thidet.shinyapps.io/thidet_louis_exo_final",
+        codeUrl: "https://github.com/lthidet/mobilites_scolaires_academie_creteil"
+    },
+    {
+        title: "Modulable",
+        title_en: "Modulable",
+        description: "Logiciel low-code ayant la particularité de permettre à l'utilisateur de créer lui-même son propre environnement de développement low-code, pour le type d'application qu'il souhaite créer.",
+        description_en: "Low-code software that allows users to build their own custom low-code development environment tailored to the type of application they wish to create.",
+        imageUrl: "img/projets/modulable.jpg",
+        tags: ["Spring", "Microservices", "Low-code"],
+        tags_en: ["Spring", "Microservices", "Low-code"]
+    },
+];
